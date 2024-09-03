f4cee783cc1f0c9c3ae490cebbc332a34bde9ae8
vatsal-afk <vtsl5819@gmail.com>
2024-09-03 22:21:05 +0530

    handle file meta data

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs=require('fs');
 const path=require('path');
+const crypto = require('crypto');
+const zlib = require('zlib');
+const sharp = require('sharp');
 
 function init() {
     if (fs.existsSync('.pix')) {
@@ -26,12 +29,49 @@ function init() {
     fs.writeFileSync(path.join(pixPath, 'index'), '');
     console.log('Initialized empty pixitory at', pixPath);
 }
-init();
 
-function add() {
-    const srcDir="./";
-    const destDir="./objects/";
+function add(filename) {
+    const srcDir = `./${filename}`;
+    const destDir = path.join("./.pix/objects", path.basename(filename));
+    function errorHandler(err) {
+        if (err) {
+            console.log('Error moving file!', err);
+        } else {
+            console.log(`${filename} successfully added to .pix/objects/`);
+        }
+    }
+    processMetadata(filename);
+    fs.copyFile(srcDir, destDir, fs.constants.COPYFILE_EXCL, errorHandler);
 }
+
+function processMetadata(filename) {
+    sharp(filename)
+    .metadata()
+    .then(metadata => {
+        const EXIFdata = JSON.stringify(metadata);
+        zlib.deflate(EXIFdata, (err, compressed) => {
+            if (err) {
+                console.error('Error compressing metadata:', err);
+                return;
+            }
+            const hash = crypto.createHash('sha1').update(compressed).digest('hex');
+            console.log("Generated Hash:", hash);
+            const objectDir = path.join(".pix/objects", hash.substring(0, 2));
+            const objectFile = path.join(objectDir, hash.substring(2));
+            if (!fs.existsSync(objectDir)) {
+                fs.mkdirSync(objectDir, { recursive: true });
+            }
+            fs.writeFileSync(objectFile, compressed);
+            console.log(`Metadata stored at ${objectFile}`);
+        });
+    })
+    .catch(err => {
+        console.error('Error extracting metadata:', err);
+    });
+}
+
+init();
+add("sample.jpg");
 
 function commit() {
 
